800d7f1b09353395f25f66bcf39b07c0a1a85096
youngeun1207 <yesheen98@gmail.com>
2022-08-02 18:07:31 +0900

    add save

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 var submit = document.getElementById('submitButton');
 submit.onclick = showImage;     //Submit 버튼 클릭시 이미지 보여주기
 
+var num = 1;
+
 function showImage() {
-    var newImage = document.getElementById('image-show').lastElementChild;
+    var image_num = 'image-show' + String(num);
+    var newImage = document.getElementById(image_num).lastElementChild;
     newImage.style.visibility = "visible";
     document.getElementById('fileName').textContent = null;     //기존 파일 이름 지우기
+    num++;
+    if (num == 5) {
+        num = 1;
+    }
 }
 
 
@@ -24,6 +31,8 @@ function loadFile(input) {
     newImage.style.visibility = "hidden";   //버튼을 누르기 전까지는 이미지 숨기기
     newImage.style.objectFit = "cover";
 
-    var container = document.getElementById('image-show');
+    var image_num = 'image-show' + String(num);
+    var container = document.getElementById(image_num);
     container.appendChild(newImage);
+
 };
