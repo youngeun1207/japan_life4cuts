abf4e34eeda768462a95a0a8c19a81a1eeeea7b7
youngeun1207 <yesheen98@gmail.com>
2022-08-02 19:10:50 +0900

    사진 컷 별 업로드 구현

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-// var submit = document.getElementById('submitButton');
-// submit.onclick = showImage;     //Submit 버튼 클릭시 이미지 보여주기
-
-// function showImage() {
-//     var image_num = 'image-show' + String(num);
-//     var newImage = document.getElementById(image_num).lastElementChild;
-//     newImage.style.visibility = "visible";
-//     document.getElementById('fileName').textContent = null;     //기존 파일 이름 지우기
-//     num++;
-//     if (num == 5) {
-//         num = 1;
-//     }
-// }
-
-
 function loadFile(input, num) {
     console.log(num);
     var file = input.files[0];
