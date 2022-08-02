b13c43a3dd5d9ee4a6efad69eb9f22592e04c248
youngeun1207 <yesheen98@gmail.com>
2022-08-02 14:13:09 +0900

    edit id

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 var submit = document.getElementById('submitButton');
 submit.onclick = showImage;     //Submit 버튼 클릭시 이미지 보여주기
 
-var num = 1;
-
 function showImage() {
-    var image_id = 'image-show' + String(num);
-    num++;
-    var newImage = document.getElementById(image_id).lastElementChild;
+    var newImage = document.getElementById('image-show').lastElementChild;
     newImage.style.visibility = "visible";
 
     // document.getElementById('image-upload').style.visibility = 'hidden';
@@ -31,7 +27,6 @@ function loadFile(input) {
     newImage.style.visibility = "hidden";   //버튼을 누르기 전까지는 이미지 숨기기
     newImage.style.objectFit = "cover";
 
-    var image_id = 'image-show' + String(num);
-    var container = document.getElementById(image_id);
+    var container = document.getElementById('image-show');
     container.appendChild(newImage);
 };
